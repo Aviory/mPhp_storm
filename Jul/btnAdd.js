7bdeb276bff8f46bede3ce608ce4115f5ed6fdb1
user <user>
2016-04-03 19:38:29 +0300

    edit ingridient and cooking text ready

--- Jul/btnAdd.js
+++ Jul/btnAdd.js
@@ -1,81 +1,109 @@
 /**
  * Created by user on 01.04.2016.
  */
-function addRecept(){
-    var edit = document.getElementById("edits");
-    edit.innerHTML="";
+var newRecept
+{
+    function addRecept() {<!-- форма для добавления нового рецепта -->
+        var edit = document.getElementById("edits");
+        edit.innerHTML = "";
 
-    var h2_Ingridients = document.createElement("h2");
-    h2_Ingridients.innerText = "Ingridients";
-    edit.appendChild(h2_Ingridients);
+        var h2_Ingridients = document.createElement("h2");
+        <!-- создание заголовок ингридиенты -->
+        h2_Ingridients.innerText = "Ingridients";
+        edit.appendChild(h2_Ingridients);
 
-    var newTxtIng    = document.createElement('textarea');
-    newTxtIng.setAttribute("id","newIngridient");
-    newTxtIng.rows   = 11;
-    newTxtIng.cols   = 35;
-    newTxtIng.maxLength = 500;
-    edit.appendChild(newTxtIng);
+        var newTxtIng = document.createElement('textarea');
+        <!-- создание нгридиенты блок -->
+        newTxtIng.setAttribute("id", "newIngridient");
+        newTxtIng.rows = 11;
+        newTxtIng.cols = 35;
+        newTxtIng.maxLength = 500;
+        edit.appendChild(newTxtIng);
 
-    var h2_Cooking = document.createElement("h2");
-    h2_Cooking.innerText = "Cooking";
-    edit.appendChild(h2_Cooking);
+        var h2_Cooking = document.createElement("h2");
+        <!-- создание риготовление заголовок -->
+        h2_Cooking.innerText = "Cooking";
+        edit.appendChild(h2_Cooking);
 
-    var newTxtCook   = document.createElement('textarea');
-    newTxtCook.setAttribute("id","newCooking");
-    newTxtCook.rows  = 15;
-    newTxtCook.cols  = 100;
-    newTxtIng.maxLength = 3000;
-    edit.appendChild(newTxtCook);
+        var newTxtCook = document.createElement('textarea');
+        <!-- создание приготовление блок -->
+        newTxtCook.setAttribute("id", "newCooking");
+        newTxtCook.rows = 15;
+        newTxtCook.cols = 100;
+        newTxtIng.maxLength = 3000;
+        edit.appendChild(newTxtCook);
 
-    var newBtnAdd     = document.createElement("button");
-    newBtnAdd.setAttribute("onclick", "btnAdd()");
-    newBtnAdd.innerText = "Ok";
-    edit.appendChild(newBtnAdd);
-}
-function btnAdd(){
-    var main    = document.getElementById("main");
-
-    var section = document.createElement("article");
-    main.appendChild(section);
-
-    var headerName = document.createElement("h1");
-    headerName.innerText = "Header name rec";
-    section.appendChild(headerName);
-
-    var ingridient   = document.createElement("article");
-    ingridient.class = "ingridients";
-    section.appendChild(ingridient);
-    var h2_Ingridients = document.createElement("h2");
-    h2_Ingridients.innerText = "Ingridients";
-    ingridient.appendChild(h2_Ingridients);
-    ingridient.appendChild(add_ul());
-
-    var cooking   = document.createElement("article");
-    cooking.class = "Cooking";
-    section.appendChild(cooking);
-    var h2_Cooking = document.createElement("h2");
-    h2_Cooking.innerText = "Cooking";
-    cooking.appendChild(h2_Cooking);
-    var p = document.createElement("p");
-    p.innerText = "text";
-    cooking.appendChild(p);
-
-    var edit = document.getElementById("edits");
-    edit.innerHTML="";
-
-    var newBtnAdd     = document.createElement("button");
-    newBtnAdd.setAttribute("onclick", "addRecept()");
-    newBtnAdd.innerText = "new recept";
-    edit.appendChild(newBtnAdd);
-}
-function add_ul(){
-    var ul     = document.createElement("ul");
-    var strIng = $('#newIngridient').val().split(/[\n\r]+/);
-
-    for(var i=0;i<strIng.length;i++){
-        var li = document.createElement("li");
-        li.innerText = strIng[i];
-        ul.appendChild(li);
+        var newBtnAdd = document.createElement("button");
+        <!-- кнопка добавления -->
+        newBtnAdd.setAttribute("onclick", "btnAdd()");
+        newBtnAdd.innerText = "Ok";
+        edit.appendChild(newBtnAdd);
     }
-    return ul;
+
+    function btnAdd() {<!-- кнопка добавить новый рецепт -->
+        var main = document.getElementById("main");
+
+        var section = document.createElement("article");
+        <!-- внешний артикль всего блока -->
+        main.appendChild(section);
+
+        var headerName = document.createElement("h1");
+        <!-- заголовок рецепта -->
+        headerName.innerText = "Header name rec";
+        section.appendChild(headerName);
+
+        var ingridient = document.createElement("article");
+        <!-- внутринний артикль ингридиентов -->
+        ingridient.setAttribute("class", "ingridients");
+        section.appendChild(ingridient);
+        var h2_Ingridients = document.createElement("h2");
+        h2_Ingridients.innerText = "Ingridients";
+        ingridient.appendChild(h2_Ingridients);
+        ingridient.appendChild(add_ul());
+
+        var cooking = document.createElement("article");
+        <!-- внутренний артикль приготовления -->
+        cooking.setAttribute("class", "cooking");
+        section.appendChild(cooking);
+        var h2_Cooking = document.createElement("h2");
+        h2_Cooking.innerText = "Cooking";
+        cooking.appendChild(h2_Cooking);
+        cooking.appendChild(addCooking());
+
+        var edit = document.getElementById("edits");
+        <!-- очистить поле добавления и добавить кнопку -->
+        edit.innerHTML = "";
+
+        var newBtnAdd = document.createElement("button");
+        newBtnAdd.setAttribute("onclick", "addRecept()");
+        newBtnAdd.innerText = "new recept";
+        edit.appendChild(newBtnAdd);
+    }
+
+    function add_ul() {<!-- пробег по ингридиентам -->
+        var ul = document.createElement("ul");
+        var strIng = $('#newIngridient').val().split(/[\n\r]+/);
+
+        for (var i = 0; i < strIng.length; i++) {
+            var li = document.createElement("li");
+            if (i > 0) {
+                li.innerText = strIng[i];
+                ul.appendChild(li);
+            }
+            if (i == 0) {
+                document.getElementById('main').lastElementChild.firstElementChild.innerText = strIng[i];
+            }
+        }
+        return ul;
+    }
+
+    function addCooking() {<!-- пробег по приготовлению -->
+        var p = document.createElement("p");
+        var strCooking = $('#newCooking').val().split(/[\n\r]+/);
+        for (var i = 0; i < strCooking.length; i++) {
+            p.innerHTML += strCooking[i];
+            p.innerHTML += "<br/>";
+        }
+        return p;
+    }
 }
