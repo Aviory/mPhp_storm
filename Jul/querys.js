a4bb22e15bef3b525ac93e2f21966ec45249cb8c
user <user>
2016-04-19 17:09:53 +0300

    ready query but not ready arrays in the query

--- Jul/querys.js
+++ Jul/querys.js
@@ -12,17 +12,18 @@ function qpost(){
     xhr.onreadystatechange = function() { // Ждём ответа от сервера
         if (xhr.readyState == 4) { // Ответ пришёл
             if(xhr.status == 200) { // Сервер вернул код 200 (что хорошо)
-                 data = xhr.responseText; // Выводим ответ сервера
-                //document.getElementById("edits").innerText = xhr.responseText;
+                data = xhr.responseText; // Выводим ответ сервера
+                document.getElementById("edits").innerText = xhr.responseText;
+                var mjson = JSON.stringify(data);
+                var reclist = JSON.parse(mjson);
+                for(var i = 0; i < reclist.length; i++)
+                {
+                    var row = rowRecipeAndFillData(reclist[i]);
+                    document.getElementById("main").appendChild(row);
+                }
             }
         }
     };
-    var mjson = JSON.stringify(data);
-    var reclist = JSON.parse(mjson);
-    for(var i = 0; i < reclist.length; i++)
-    {
-        var row = rowRecipeAndFillData(reclist[i]);
-        document.getElementById("main").appendChild(row);
-    }
+
 }
 
